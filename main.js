2f6328309de00190988490deac17149a2ce8022a
Diablohu <Diablohu@users.noreply.github.com>
2025-07-02 08:43:28 +0800

    commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import App from "./src/app.js";
+
+const argv = yargs(hideBin(process.argv)).parse();
+
+const start = async () => {
+    const app = new App({
+        port: process.env.AKIGUMO_PORT || argv.port || 8081,
+    });
+    await app.start();
+};
+
+start().catch((err) => {
+    console.error(err);
+});
--- start.js
+++ start.js
@@ -1,17 +1,63 @@
-import "dotenv/config";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import App from "./src/app.js";
+import { spawn, execSync } from "node:child_process";
+import dayjs from "dayjs";
+import { select, Separator } from "@inquirer/prompts";
+import npmRunScript from "npm-run-script";
+import p from "./package.json" with { type: "json" };
 
-const argv = yargs(hideBin(process.argv)).parse();
-
-const start = async () => {
-    const app = new App({
-        port: process.env.AKIGUMO_PORT || argv.port || 8081,
+async function main() {
+    const answer = await select({
+        message: "请选择一个任务",
+        pageSize: 20,
+        choices: [
+            new Separator(" "),
+            new Separator("── 🚧 本地开发 ──────────"),
+            {
+                name: "开启服务器",
+                short: "\n🚧 开启服务器",
+                value: "npm:start",
+                description: "npm 命令: start\n",
+            },
+            new Separator(" "),
+            new Separator("── 🚀 线上发布 ──────────"),
+            {
+                name: "正式",
+                short: "\n🚀 线上发布：正式",
+                value: "publish:release",
+                description: "触发线上发布流程\n",
+            },
+            {
+                name: "预览",
+                short: "\n🚀 线上发布：预览",
+                value: "publish:preview",
+                description: "触发线上发布流程\n",
+            },
+            new Separator(" "),
+        ],
     });
-    await app.start();
-};
 
-start().catch((err) => {
-    console.error(err);
+    console.log(" ");
+
+    const [type, command] = answer.split(":");
+
+    switch (type) {
+        case "npm":
+            npmRunScript(p.scripts[command], { stdio: "inherit" });
+            break;
+        case "publish":
+            const status = execSync("git status --porcelain").toString().trim();
+            if (status) {
+                console.error("⛔ 请先提交本地的改动！");
+                return;
+            }
+            const tag = `publish-${command}-${dayjs().format(
+                `YYYYMMDD`
+            )}-${dayjs().format(`HHmmss`)}`;
+            spawn(`git`, ["tag", tag], { stdio: "inherit" });
+            spawn(`git`, ["push", "origin", tag], { stdio: "inherit" });
+            break;
+    }
+}
+
+await main().catch((err) => {
+    console.trace(err);
 });
